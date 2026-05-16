7910cde0ea8c662987a6d62a1ef3be40198a09db
InasAlSaabb <inassaab729@gmail.com>
2026-05-16 21:14:21 +0300

    inas

--- next.config.js
+++ next.config.js
@@ -1,5 +1,42 @@
-const nextConfig = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withBundleAnalyzer({
+  // Choose output mode based on env variables
+  output:"standalone",
+
   reactStrictMode: true,
-};
+
+  pageExtensions: ["ts", "tsx", "js"],
+
+  eslint: {
+    dirs: ["src"],
+  },
+
+  images: {
+    domains: ["flagcdn.com"], // no https://
+  },
+
+  webpack: (config) => {
+    // Handle SVGs with SVGR
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+
+    // Polyfill Node.js modules that are not available in browser
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      dns: false,
+      child_process: false,
+      tls: false,
+    };
+
+    return config;
+  },
+});
 
 module.exports = nextConfig;
